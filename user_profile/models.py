f38201f430c00d14a9770a87a4043702a7238ef5
raicodev <joyner.7804@gmail.com>
2023-08-07 15:50:58 -0400

    Se colocaron opcionales los campos que no son importantes

--- user_profile/models.py
+++ user_profile/models.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import AbstractUser
 # Se hereda de la base del modelo usuario, reescribiendo el modelo que tiene por defecto
 # django
 class User(AbstractUser):
-    image = models.ImageField(verbose_name="Imagen de perfil", upload_to="profile_img")
-    nick = models.CharField(verbose_name="Apodo", max_length=50)
-    bio = models.TextField(verbose_name="Descripcion o motivos de registro")
+    image = models.ImageField(verbose_name="Imagen de perfil", upload_to="profile_img", blank=True)
+    nick = models.CharField(verbose_name="Apodo", max_length=50, blank=True)
+    bio = models.TextField(verbose_name="Descripcion o motivos de registro", blank=True)
 
     class Meta:
         db_table = "user"
