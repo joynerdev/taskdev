d9e6dbf849d9ce7bf60622dd54f6e0aee8d19924
raicodev <joyner.7804@gmail.com>
2023-08-07 15:39:38 -0400

    Modificacion y creacion del modelo de usuario Personalizado

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# Se hereda de la base del modelo usuario, reescribiendo el modelo que tiene por defecto
+# django
+class User(AbstractUser):
+    image = models.ImageField(verbose_name="Imagen de perfil", upload_to="profile_img")
+    nick = models.CharField(verbose_name="Apodo", max_length=50)
+    bio = models.TextField(verbose_name="Descripcion o motivos de registro")
