1de77962e5532f28bc2ce783ab8da5a242f10e5b
raicodev <joyner.7804@gmail.com>
2023-08-07 16:14:00 -0400

    Registro de la ruta de inicio de sesion

--- login/urls.py
+++ login/urls.py
@@ -4,6 +4,7 @@ from .views import *
 app_name = 'login'
 
 urlpatterns = [
+    path('', SignInView.as_view(), name='loginProfile'),
     path('register', RegisterProfile.as_view(), name='newProfile'),
     path('finish', SecondDataProfile.as_view(), name='dataProfile'),
     
