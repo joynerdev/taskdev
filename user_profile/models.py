579c2f0d99fb622c1b6546ad191c81b24b92392c
raicodev <joyner.7804@gmail.com>
2023-08-08 10:40:47 -0400

    Se registro una funcion para asignar un Uid a cada imagen que se suba

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,10 +1,23 @@
+import os
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Se hereda de la base del modelo usuario, reescribiendo el modelo que tiene por defecto
 # django
+
+def get_image_path(instance, filename):
+    # Genera un UID único
+    uid = uuid.uuid4()
+    # Obtiene la extensión original del archivo
+    extension = os.path.splitext(filename)[1]
+    # Define la nueva ruta del archivo de la imagen con el UID como nombre y la extensión original
+    new_filename = f'profile_img/{uid}{extension}'
+    return new_filename
+
+
 class User(AbstractUser):
-    image = models.ImageField(verbose_name="Imagen de perfil", upload_to="profile_img", default='profile_img/profile_default.png')
+    image = models.ImageField(verbose_name="Imagen de perfil", upload_to=get_image_path, blank=True)
     nick = models.CharField(verbose_name="Apodo", max_length=50, blank=True)
     bio = models.TextField(verbose_name="Descripcion o motivos de registro", blank=True)
 
