b217c61c7d89b0e932abddc96581568fec8d114f
raicodev <joyner.7804@gmail.com>
2023-08-08 17:36:05 -0400

    Se optimizan las vistas y se aplica el uso de los mixins creados

--- login/views.py
+++ login/views.py
@@ -1,28 +1,52 @@
-from django.shortcuts import render
-
 from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.models import User
-from django.views.generic import CreateView, TemplateView, UpdateView
-from .forms import RegisterProfileForm
+from django.contrib.auth import authenticate, login
+from django.views.generic import CreateView, UpdateView
+
+from functions.mixins import ContextMixin, IsUserAunthenticated, RestrictSecondPageRegister
+from .forms import RegisterProfileForm, SecondStepRegisterForm
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 
 # Vista de Inicio de Sesion
-class SignInView(LoginView):
+class SignInView(ContextMixin, LoginView):
     template_name = 'login.html'
     redirect_authenticated_user = True
+    custom_context = {
+        'app_name': 'Iniciar Sesion'
+    }
 
-class SignOutView(LogoutView):
+class SignOutView(LoginRequiredMixin, LogoutView):
     pass
 
-class RegisterProfile(TemplateView):
+class RegisterProfile(IsUserAunthenticated, ContextMixin, CreateView):
+    form_class = RegisterProfileForm
+    success_url = reverse_lazy('login:dataProfile')
     template_name = 'register.html'
+    custom_context = {
+        'app_name': 'Registrar nuevo usuario'
+    }
     
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["form_register"] = RegisterProfileForm()
-        context["app_name"] = 'Nuevo Usuario'
-        return context
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return response
 
 
-class SecondDataProfile(TemplateView):
+class SecondDataProfile(LoginRequiredMixin, RestrictSecondPageRegister, ContextMixin, UpdateView):
+    form_class = SecondStepRegisterForm
     template_name = "data_profile.html"
+
+    success_url = reverse_lazy('login:dataProfile')
+
+    custom_context = {
+        'app_name': 'Finalizar registro de perfil',
+        'title_container': 'Ultimos ajustes a tu perfil 👷'
+    }
+
+    def get_object(self):
+        return self.request.user
