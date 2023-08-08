3c19cb11f88980df17be5c8f314941532caeda32
raicodev <joyner.7804@gmail.com>
2023-08-08 09:52:10 -0400

    Se agrego la imagen por defecto del usuario

--- user_profile/models.py
+++ user_profile/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 # Se hereda de la base del modelo usuario, reescribiendo el modelo que tiene por defecto
 # django
 class User(AbstractUser):
-    image = models.ImageField(verbose_name="Imagen de perfil", upload_to="profile_img", blank=True)
+    image = models.ImageField(verbose_name="Imagen de perfil", upload_to="profile_img", default='profile_img/profile_default.png')
     nick = models.CharField(verbose_name="Apodo", max_length=50, blank=True)
     bio = models.TextField(verbose_name="Descripcion o motivos de registro", blank=True)
 
