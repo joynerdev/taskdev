fa06583d7ac1c4d79566cb70f84d39ef7deceff6
raicodev <joyner.7804@gmail.com>
2023-08-08 11:45:26 -0400

    Registro del archivo de rutas de Tasks en el archivo principal

--- config/urls.py
+++ config/urls.py
@@ -22,5 +22,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('login/', include('login.urls'))
+    path('login/', include('login.urls')),
+    path('tasks/', include('tasks.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
