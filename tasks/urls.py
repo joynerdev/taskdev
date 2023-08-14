2b410fd3d8a4c53678a29b1aa1f01bcbc1462b88
raicodev <joyner.7804@gmail.com>
2023-08-13 20:56:18 -0400

    Se registraron las rutas de las respectivas vistas de las tareas

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,4 +5,9 @@ app_name = 'task'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('importantes', TasksImportants.as_view(), name='important'),
+    path('basicas', TasksPending.as_view(), name='basic'),
+    path('pospuestas', TasksPostponed.as_view(), name='postponed'),
+    path('incompletas', TasksIncomplete.as_view(), name='incomplete')
+    
 ]
