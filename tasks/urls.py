7b07ac540d8f755bfe73e753fb252b1179ae9d2d
raicodev <joyner.7804@gmail.com>
2023-08-15 10:07:48 -0400

    Se solucionaron conflictos en las rutas y se registro una nueva (Nueva tarea)

--- tasks/urls.py
+++ tasks/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('importantes', TasksImportants.as_view(), name='important'),
     path('basicas', TasksPending.as_view(), name='basic'),
     path('pospuestas', TasksPostponed.as_view(), name='postponed'),
-    path('incompletas', TasksIncomplete.as_view(), name='incomplete')
+    path('incompletas', TasksIncomplete.as_view(), name='incomplete'),
+    path('nueva', NewTask.as_view(), name='new')
     
 ]
